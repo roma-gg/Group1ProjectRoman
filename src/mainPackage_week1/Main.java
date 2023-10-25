bb4fe03434cc036805ed5f9bac970eacbe6eaeec
roma-gg <rbaidachenko@gmail.com>
2023-10-25 17:16:55 +0100

    Test for pull request

--- src/mainPackage_week1/Main.java
+++ src/mainPackage_week1/Main.java
@@ -7,19 +7,18 @@ import Roman.PracticeTasks;
 public class Main {
     public static void main(String[] args) {
 
+        //test for pull request
+
         // String-3 > countYZ (Roman)
-        System.out.println("String-3 > countYZ");
+        System.out.println("String-3 > countYZ (Roman)");
+
         System.out.println("Test case 1:");
         System.out.println(PracticeTasks.countYZ("fez day") == 2);
+
         System.out.println("Test case 2:");
         System.out.println(PracticeTasks.countYZ("day fez") == 2);
+
         System.out.println("Test case 3:");
         System.out.println(PracticeTasks.countYZ("day fyyyz") == 2);
-
-        //space for rest of tasks
-        // test module issue
-
-
-
     }
 }
