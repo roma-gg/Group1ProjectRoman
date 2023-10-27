4fc78f3456395813b3c49511fda00bed20e02f30
waniusha <ion.nicolaescu15@icloud.com>
2023-10-27 19:28:31 +0100

    trying to fix the structure

---
+++ src/Ion/ClassTask.java
@@ -0,0 +1,22 @@
+package Ion;
+
+public class ClassTask {
+
+    public boolean endOther(String a, String b){
+        if(a.toLowerCase().endsWith(b.toLowerCase()) || b.toLowerCase().endsWith(a.toLowerCase())){
+            return true;
+        }else return false;
+    }
+
+    /*
+    Given two strings, return true if either of the strings appears at the very end of the other string,
+    ignoring upper/lower case differences (in other words, the computation should not be "case sensitive").
+    Note: str.toLowerCase() returns the lowercase version of a string.
+
+
+endOther("Hiabc", "abc") → true
+endOther("AbC", "HiaBc") → true
+endOther("abc", "abXabc") → true
+     */
+
+}
