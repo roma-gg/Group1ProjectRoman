bf54d16fb3871b814ccb300e9d4071d07abf66ff
Yaseminc0 <yasemincalik77@gmail.com>
2023-10-27 19:18:58 +0100

    requested task added

--- src/Yasemin/Task01_xyzThere.java
+++ src/Yasemin/Task01_xyzThere.java
@@ -1,4 +1,16 @@
 package Yasemin;
 
 public class Task01_xyzThere {
+    /*
+    Return true if the given string contains an appearance of "xyz"
+    where the xyz is not directly preceeded by a period (.). So "xxyz" counts
+    but "x.xyz" does not.
+
+    xyzThere("abcxyz") → true
+    xyzThere("abc.xyz") → false
+    xyzThere("xyz.abc") → true
+     */
+
+
+
 }
