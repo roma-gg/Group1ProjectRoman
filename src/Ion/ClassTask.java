1e0367d8e9175e1d5eeeea15b2be554f7c91f974
waniusha <ion.nicolaescu15@icloud.com>
2023-10-27 19:29:26 +0100

    Solved  assigned task

--- src/Ion/ClassTask.java
+++ src/Ion/ClassTask.java
@@ -5,7 +5,9 @@ public class ClassTask {
     public boolean endOther(String a, String b){
         if(a.toLowerCase().endsWith(b.toLowerCase()) || b.toLowerCase().endsWith(a.toLowerCase())){
             return true;
-        }else return false;
+        }else {
+            return false;
+        }
     }
 
     /*
