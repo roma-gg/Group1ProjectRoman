cdc7e74b3ba2d12ff300e99e4eba3cafa870f3f7
azizemilbekuulu <140080207+azizemilbekuulu@users.noreply.github.com>
2023-10-29 13:37:35 +0000

    Created Class and initialised ArrayList

--- src/AzizPackage2/Aziz_LoopTask.java
+++ src/AzizPackage2/Aziz_LoopTask.java
@@ -1,5 +1,29 @@
 package AzizPackage2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Aziz_LoopTask {
+    public static void main(String[] args) {
+        ArrayList<String> namesOf = new ArrayList<>();
+        namesOf.addAll(Arrays.asList("Anna", "Mike", "Aliya", "Donald", "Muhtar", "Remus", "Mehmet","Asya"));
+        System.out.println(namesOf);
+    }
+
+    public static int count(ArrayList<String> names){
+        int count = 0;
+        for (String eachName : names) {
+            if(eachName.charAt(0) == eachName.charAt(eachName.length()-1)){
+                count++;
+            }
+        }
+        return count;
+
+    }
 
 }
+/*
+Task 3 : Write a program that accepts String array.Count how many names have the same first and last letter
+                Input : "Anna", "Mike", "Aliya", "Donald", "Muhtar", "Remus", "Mehmet","Asya"
+                Output : 4
+ */
