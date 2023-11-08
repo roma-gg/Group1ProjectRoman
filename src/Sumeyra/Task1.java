8039c73d5c3f77e7c7bd28027d0ed50eacc161cb
Sumeyra <sumozgun@gmail.comgit config --global user.password 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>
2023-11-08 19:13:26 +0000

    Task 1 created and tested

---
+++ src/Sumeyra/Task1.java
@@ -0,0 +1,33 @@
+package Sumeyra;
+/*
+
+Return true if the given string contains a "bob" string, but where the middle 'o' char can be any char.
+
+
+bobThere("abcbob") → true
+bobThere("b9b") → true
+bobThere("bac") → false
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Test case 1: ");
+        System.out.println(isBobHere("abcbob"));
+        System.out.println("Test case 2: ");
+        System.out.println(isBobHere("b9b"));
+        System.out.println("Test case 3: ");
+        System.out.println(!isBobHere("bac"));
+    }
+
+    public static boolean isBobHere(String str) {
+
+        boolean bob=false;
+        for(int i=0;i<str.length()-2;i++) {
+            if (str.substring(i, i + 1).equals("b") && str.substring(i + 2, i + 3).equals("b")) {
+                bob = true;
+            }
+        }
+
+
+        return bob;
+    }
+}
