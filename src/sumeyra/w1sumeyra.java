8611534e97d8aa2a704483ed28bcf029973fa933
Sumeyra <sumozgun@gmail.comgit config --global user.password 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>
2023-11-08 23:51:50 +0000

    added solution of q1

--- src/sumeyra/w1sumeyra.java
+++ src/sumeyra/w1sumeyra.java
@@ -1,4 +1,19 @@
-package sumeyra;
+package Sumeyra;
 
 public class w1sumeyra {
+
+    public static void main(String[] args) {
+
+        identify(5);
+        identify(18);
+    }
+
+
+    public static void identify(int number){
+        if(number%2==0){
+            System.out.println("even");
+        }else{
+            System.out.println("odd");
+        }
+    }
 }
