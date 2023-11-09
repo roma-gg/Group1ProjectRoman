8651ce33785996067d216e455afeb91b662d77ce
Sumeyra <sumozgun@gmail.comgit config --global user.password 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>
2023-11-09 00:50:45 +0000

    updated q1 solution

--- src/sumeyra/w1sumeyra.java
+++ src/sumeyra/w1sumeyra.java
@@ -9,11 +9,17 @@ public class w1sumeyra {
     }
 
 
-    public static void identify(int number){
-        if(number%2==0){
-            System.out.println("even");
-        }else{
-            System.out.println("odd");
-        }
+    public static void identify(int number) {
+
+//way1
+//        if(number%2==0){
+//            System.out.println("even");
+//        }else{
+//            System.out.println("odd");
+//        }
+//    }
+//way2
+        String str = (number % 2 == 0) ? "even" : "odd";
+        System.out.println(str);
     }
 }
