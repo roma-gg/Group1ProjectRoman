d62d3afe2793d2fd07771c7b211c842e30e848bd
waniusha <ion.nicolaescu15@icloud.com>
2023-11-15 20:48:02 +0000

    Added testClass

---
+++ src/Ion/hkmTask/Test.java
@@ -0,0 +1,16 @@
+package Ion.hkmTask;
+
+import static Ion.hkmTask.FinRa.finRa;
+import static Ion.hkmTask.OddEven.oddEven;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        finRa();
+        System.out.println(oddEven(2));
+        System.out.println(oddEven(5));
+
+    }
+
+}
