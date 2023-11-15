081b04f8ba3b040e638628b2369925887488de82
Sumeyra <sumozgun@gmail.comgit config --global user.password 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>
2023-11-15 20:59:27 +0000

    updated

--- src/Sumeyra/w1Sumeyra/Task2.java
+++ src/Sumeyra/w1Sumeyra/Task2.java
@@ -1,12 +1,22 @@
 package Sumeyra.w1Sumeyra;
 
     public class Task2 {
+        public static void main(String[] args) {
 
+           // divideWithoutOperator(5, 10);
+            divideWithoutOperator(10,5);
+        }
 
         public static void divideWithoutOperator(int divident, int divisor) {
 
             int quotient = 0;
             int remainder = 0;
+
+            if (divident < divisor) {
+                System.err.println("Divident must be bigger than divisor");
+                System.exit(1);
+            }
+
             while (divident >= divisor) {
                 divident = divident - divisor;
                 quotient++;
@@ -14,15 +24,9 @@ package Sumeyra.w1Sumeyra;
             }
 
             System.out.println("quotient = " + quotient + ", remainder = " + remainder);
-// could not achieve
-//        if (divident < divisor) {
-//            System.err.println("Divident must be bigger than divisor");
-//            System.exit(1);
-//        }
+
 
         }
-
-
     }
 
 
