e3614fba9ae2e42eef0515f7175294488b5159b6
damlagece <damlagece@hotmail.com>
2023-11-16 13:52:40 +0000

    Task done

--- src/damla/hkmTask/FINRA.java
+++ src/damla/hkmTask/FINRA.java
@@ -6,6 +6,7 @@ public class FINRA {
 
         for (int i = 1; i <= 30; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
+
                 System.out.println("FINRA");
             } else if (i % 3 == 0) {
                 System.out.println("FIN");
