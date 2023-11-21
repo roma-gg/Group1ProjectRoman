ebf48b5994e84bd622c408948afa5a8b08c03dee
Sumeyra <sumozgun@gmail.comgit config --global user.password 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>
2023-11-21 23:24:51 +0000

    ...

---
+++ src/Sumeyra/w2/ConsecutiveNumbers.java
@@ -0,0 +1,31 @@
+package Sumeyra.w2;
+
+
+public class ConsecutiveNumbers {
+    public static void main(String[] args) {
+
+//        int n =50;
+//
+//        String result = "";
+//        for(int i = 1; i <= n; i++){
+//
+//            if(i % 2 == 0 && i % 5 == 0){
+//                result += "CodilityCoders";
+//            }if(i % 2 == 0 && i % 3 == 0){
+//                result += "CodilityTest";
+//            }if(i % 2 == 0){
+//                result += "Codility";
+//            }if(i % 3 == 0){
+//                result += "Test";
+//            }if(i % 5 == 0){
+//                result += "Coders";
+//            }if(!(i%2==0 || i%3==0 || i%5==0)){
+//                System.out.println(i);
+//            }
+//            System.out.println(result);
+//        }
+
+
+
+    }
+}
