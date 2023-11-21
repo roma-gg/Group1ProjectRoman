cc084d48cf66f5b9833a592cac669c1c9120fdf2
Yaseminc0 <yasemincalik77@gmail.com>
2023-11-21 09:54:47 +0000

    created test class and tested task1

---
+++ src/Yasemin/MentorTasks/TestClass.java
@@ -0,0 +1,14 @@
+package Yasemin.MentorTasks;
+
+import static Yasemin.MentorTasks.OddEvenNumber.identify;
+
+public class TestClass {
+
+    public static void main(String[] args) {
+
+        System.out.println(identify(5));
+        System.out.println(identify(5));
+
+
+    }
+}
