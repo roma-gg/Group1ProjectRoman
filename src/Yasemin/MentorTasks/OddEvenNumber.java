29242f07cb58a53a7e6812b35c9a538761182163
Yaseminc0 <yasemincalik77@gmail.com>
2023-11-21 09:54:22 +0000

    solved task odd even number

--- src/Yasemin/MentorTasks/OddEvenNumber.java
+++ src/Yasemin/MentorTasks/OddEvenNumber.java
@@ -1,4 +1,17 @@
 package Yasemin.MentorTasks;
 
 public class OddEvenNumber {
+
+    public static String identify(int num){
+
+        if(num%2==0){
+            return "even";
+        }else
+            return "odd";
+
+    }
+
 }
+/*
+write a method to identify given number is odd or even
+ */
