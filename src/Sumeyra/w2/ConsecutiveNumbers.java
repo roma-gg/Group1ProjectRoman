bbdcc1f65338a920d3491b4246e4d0ff6f9deea3
Sumeyra <sumozgun@gmail.comgit config --global user.password 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>
2023-11-22 15:18:34 +0000

    task3 is added

--- src/Sumeyra/w2/ConsecutiveNumbers.java
+++ src/Sumeyra/w2/ConsecutiveNumbers.java
@@ -4,26 +4,26 @@ package Sumeyra.w2;
 public class ConsecutiveNumbers {
     public static void main(String[] args) {
 
-//        int n =50;
-//
-//        String result = "";
-//        for(int i = 1; i <= n; i++){
-//
-//            if(i % 2 == 0 && i % 5 == 0){
-//                result += "CodilityCoders";
-//            }if(i % 2 == 0 && i % 3 == 0){
-//                result += "CodilityTest";
-//            }if(i % 2 == 0){
-//                result += "Codility";
-//            }if(i % 3 == 0){
-//                result += "Test";
-//            }if(i % 5 == 0){
-//                result += "Coders";
-//            }if(!(i%2==0 || i%3==0 || i%5==0)){
-//                System.out.println(i);
-//            }
-//            System.out.println(result);
-//        }
+        int n= 50;
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0 && i % 2 == 0) {
+                System.out.println("Codility" + "Test" + "Coders");
+            } else if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Test" + "Coders");
+            } else if (i % 2 == 0 && i % 3 == 0 ) {
+                System.out.println("Codility" + "Test");
+            } else if (i % 2 == 0 && i % 5 == 0 ) {
+                System.out.println("Codility" + "Coders");
+            }else if (i % 2 == 0 ) {
+                System.out.println("Codility");
+            }else if (i % 3 == 0 ) {
+                System.out.println("Test");
+            } else if (i % 5 == 0) {
+                System.out.println("Coders");
+            } else {
+                System.out.println(i);
+            }
+        }
 
 
 
