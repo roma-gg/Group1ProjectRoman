da087d34a2cfad47cf21d3ef7eff7caae90d86eb
bkttskrn <buket_taskiran@hotmail.com>
2023-11-23 14:07:42 +0000

    SwapNumbers solved

--- src/buket/mentorTasks/SwapNumbers.java
+++ src/buket/mentorTasks/SwapNumbers.java
@@ -1,4 +1,20 @@
 package buket.mentorTasks;
 
 public class SwapNumbers {
+
+    public static void main(String[] args) {
+
+        int a= 10;
+        int b = 20;
+        a= a +b;
+        b= a-b;
+        a=a-b;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+
+    }
+
+
 }
