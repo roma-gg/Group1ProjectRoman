d39de4c0891965e43f50014acc0a4eaf35deded1
Yaseminc0 <yasemincalik77@gmail.com>
2023-12-05 10:51:00 +0000

    check code in divide class

--- src/Yasemin/MentorTasks/W2_divideWithoutOperator.java
+++ src/Yasemin/MentorTasks/W2_divideWithoutOperator.java
@@ -4,7 +4,6 @@ public class W2_divideWithoutOperator {
 
     public static void divideWithoutOp(int num1, int num2){
         int result=0;
-        int reminder;
         while(num1>=num2) {
             result++;
             num1=num1-num2;
