423f7a6f2ac46e6648fddf6b930eb6dc2f3360b6
Yaseminc0 <yasemincalik77@gmail.com>
2023-12-14 09:09:33 +0000

    created FrequencyOf Character and added task

---
+++ src/Yasemin/MentorTasks/week_04/FrequencyOfCharacter.java
@@ -0,0 +1,35 @@
+package Yasemin.MentorTasks.week_04;
+
+public class FrequencyOfCharacter {
+    
+    public static String FrequecyString(String str){
+        String result="";
+
+        for (int j = 0; j < str.length(); j++) {
+            char ch = str.charAt(j);
+            int count = 0;
+
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == ch) {
+                    count++;
+                }
+            }
+
+            if (result.contains(""+ch)) {
+                continue;
+            }
+            result += ch +""+count;
+
+        }
+        return result;
+
+    }
+    
+    
+}
+/*
+String -- Frequency of Characters
+Write a return method that can find the frequency of 
+characters
+Ex: FrequencyOfChars("AAABBCDD") ==> A3B2C1D
+ */
